3d1b96a5b4cb026769663fa030a4444132d67e4d
Balolebwami <tchasingajacques@gmail.com>
2023-08-01 10:44:18 +0200

    new commit

---
+++ src/components/NavLinks.js
@@ -0,0 +1,11 @@
+const NavLinks = () => (
+  <div className="NavlinksLI">
+    <ul className="getNavLikns">
+      <li>Rockets</li>
+      <li className="ProfileGet">Mission</li>
+      <li>Profile</li>
+    </ul>
+  </div>
+);
+
+export default NavLinks;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import logo from '../asset/planet.png';
+import NavLinks from './NavLinks';
+import '../index.css';
 
 export default function Navbar() {
   return (
     <header>
       <nav className="nav-section">
         <img src={logo} className="space-logo" alt="logo" />
+        <NavLinks />
       </nav>
+
     </header>
   );
 }
