ee474994e8e8e8182fa086f566c52633ee1812f6
binyamT <binyampowerhc@gmail.com>
2023-08-02 16:54:58 +0300

    display rocket reservation and cancelled on Myprofile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,21 @@
-export default function Myprofile() {
+import { useSelector } from 'react-redux';
+
+const Myprofile = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const reserved = rockets.filter((rocket) => rocket.reserved === true);
+
   return (
-    <h3>This is My Profile section</h3>
+    <table className="profile-table">
+      <tr>
+        <th>My Missions</th>
+        <th>My Rockets</th>
+      </tr>
+      <tr className="data-row">
+        <td> - </td>
+        <td>{ reserved ? reserved.map((rocket) => (<p key={rocket.id} className="rocket-data-profile">{rocket.rocket_name}</p>)) : ''}</td>
+      </tr>
+    </table>
   );
-}
+};
+
+export default Myprofile;
