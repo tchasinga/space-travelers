88c11de37391609fb014f93b0515d13bb991f2a7
binyamT <binyampowerhc@gmail.com>
2023-08-02 18:44:05 +0300

    impliment active status for the navigations links

--- src/components/NavLiks.js
+++ src/components/NavLiks.js
@@ -1,15 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../index.css';
+import NavStyles from '../styles/NavBar.module.css';
 
-const NavLinks = () => (
-  <div>
-    <ul className="getNavDesigner">
-      <Link to="/">Rocket</Link>
-      <Link to="/mission">Mission</Link>
-      <span className="upbar"> | </span>
-      <Link to="/profile">My-profile</Link>
-    </ul>
-  </div>
-);
+const NavLinks = () => {
+  const location = useLocation();
+
+  const getNavLinkStyle = (pathname, style) => {
+    if (location.pathname === pathname) {
+      return `${style} ${NavStyles.active}`;
+    }
+    return style;
+  };
+
+  const rocketLinkStyle = getNavLinkStyle('/', `${NavStyles.linksStyles}  `);
+  const missionsLinkStyle = getNavLinkStyle('/mission', `${NavStyles.linksStyles}`);
+  const myProfileLinkStyle = getNavLinkStyle('/profile', `${NavStyles.linksStyles}`);
+
+  return (
+    <div>
+      <ul className="getNavDesigner">
+        <Link className={rocketLinkStyle} to="/">Rocket</Link>
+        <Link className={missionsLinkStyle} to="/mission">Mission</Link>
+        <span className="upbar"> | </span>
+        <Link className={myProfileLinkStyle} to="/profile">My-profile</Link>
+      </ul>
+    </div>
+  );
+};
 
 export default NavLinks;
